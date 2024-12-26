01af319d213548e4c54bd99435ef4dc5503dcdc3
Tope Bello <Belltop12@gmail.com>
2024-12-26 15:34:22 +0100

    fetching error fixed

--- src/Pages/Player/Player.jsx
+++ src/Pages/Player/Player.jsx
@@ -33,7 +33,7 @@ const Player = () => {
     <div className='player'>
       <img src={back_arrow_icon} alt=""onClick={()=>{navigate(-2)}}/>
       <iframe width="90%" height="90%" 
-      src={`http://www.youtube.com/embed/${apiData.key}`}
+      src={`https://www.youtube.com/embed/${apiData.key}`}
       title='trailer' frameBorder="0" allowFullScreen></iframe>
       <div className="player-info">
         <p>{apiData.published_at.slice(0,10)}</p>
